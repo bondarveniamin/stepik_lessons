4b68df2f22d5ef48b848d54fe997e33d7a6a93f9
bondarveniamin <veniaminbondar@yandex.ru>
2021-06-08 00:33:43 +0300

    AAA

--- module_4/part_1/test_items.py
+++ module_4/part_1/test_items.py
@@ -3,6 +3,11 @@ search_button_css_locator = ".btn.btn-lg.btn-primary.btn-add-to-basket"
 
 
 def test_guest_should_see_button_add_to_basket(browser):
+    # Arrange
     browser.get(link)
+
+    # Act
     answer = browser.find_element_by_css_selector(search_button_css_locator).text
+
+    # Assert
     assert len(answer) > 0
