f1866d312bd75b8e7362b59e63683c431de64a75
bondarveniamin <veniaminbondar@yandex.ru>
2021-06-27 02:20:56 +0300

    changed to explicit page initialization

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -10,9 +10,8 @@ class TestMainPage:
                         link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()  # открываем страницу
         page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-        page = LoginPage(browser, page.go_to_login_page())
-        page.should_be_login_page()
-
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
         page = MainPage(browser, link)
